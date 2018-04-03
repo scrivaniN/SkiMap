d45aabcc6d2e114a31b497b5525647ccff3f8f73
scrivaniN <n.scrivani@yahoo.com>
2018-04-03 14:25:26 -0400

    Changed marker to sunapee

--- app/src/main/java/me/jaxbot/skimap/MapsActivity.java
+++ app/src/main/java/me/jaxbot/skimap/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng sunapee = new LatLng(43.3876, -72.0879);
+        mMap.addMarker(new MarkerOptions().position(sunapee).title("Marker in Sunapee"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sunapee));
     }
 }
